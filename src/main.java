8bd79d550aace1a362bceb94503a38b0b264d853
Jamie Nolan <42931085+jamieNolanDEV@users.noreply.github.com>
2021-01-13 17:15:35 +0000

    The java

---
+++ src/main.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+
+public class main {
+    public static void main(String[] args) {
+        linkedlist list = new linkedlist();
+        list.addFront(5);
+        list.addFront(2);
+        list.addFront(6);
+        list.addFront(423);
+        list.printList();
+        list.sizeList();
+        list.sizeList();
+        list.clearList();
+
+    }
+}
